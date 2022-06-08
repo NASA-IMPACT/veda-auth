d21eee40ee3089dc39d73d7e9428a5cd7cf90d7c
Anthony Lukach <anthonylukach@gmail.com>
2022-06-08 12:34:29 -0700

    Add descriptions

--- config.py
+++ config.py
@@ -4,5 +4,23 @@ import pydantic
 
 
 class Config(pydantic.BaseSettings):
-    stage: str = pydantic.Field(default_factory=getuser)
-    owner: str = pydantic.Field(default_factory=getuser)
+    stage: str = pydantic.Field(
+        description=" ".join(
+            [
+                "Stage of deployment (e.g. 'dev', 'prod').",
+                "Used as suffix for stack name.",
+                "Defaults to current username.",
+            ]
+        ),
+        default_factory=getuser,
+    )
+    owner: str = pydantic.Field(
+        description=" ".join(
+            [
+                "Name of primary contact for Cloudformation Stack.",
+                "Used to tag generated resources",
+                "Defaults to current username.",
+            ]
+        ),
+        default_factory=getuser,
+    )
