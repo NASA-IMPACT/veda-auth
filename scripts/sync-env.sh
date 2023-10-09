cd117448bf854f26ee114673c9cec62c83e048fb
Slesa Adhikari <slesaad@gmail.com>
2023-10-09 14:09:32 -0500

    Print .env

--- scripts/sync-env.sh
+++ scripts/sync-env.sh
@@ -3,3 +3,4 @@
 
 echo Loading environment secrets from $1
 aws secretsmanager get-secret-value --secret-id $1 --query SecretString --output text | jq -r 'to_entries|map("\(.key)=\(.value|tostring)")|.[]' > .env
+cat .env
